9999ca88fa44f1c222aee01e3bbbe6b8ae73371e
Gwen <gwen.thelin@gmail.com>
2022-01-19 22:26:34 -0700

    Original Commit for this rocket game thing

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    Rigidbody rb;
+    [SerializeField] float thrust = 100f;
+    [SerializeField] float rotateThrust = 1f;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        ProcessThrust();
+        ProcessRotate();
+    }
+
+    //If player is pushing the space key move the player forward
+    void ProcessThrust() {
+        if(Input.GetKey(KeyCode.Space)) {
+            rb.AddRelativeForce(Vector3.up * thrust * Time.deltaTime);
+        }
+    }
+
+    //If the player is pushing left or right it will rotate the player. you cannot rotate both ways at the same time. I used an elf if statement on the right if statement to make this happen because that is the simplest way I have found to do it. Also the video online used it.
+    void ProcessRotate() {
+        if(Input.GetKey(KeyCode.A)) {
+            Rotate(rotateThrust);
+        }
+        else if(Input.GetKey(KeyCode.D)) { 
+            Rotate(-rotateThrust);
+        }
+    }
+
+    void Rotate(float rotateThisFrame) {
+        //Freezing rotation so we can manually rotate without bugs
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotateThisFrame * Time.deltaTime);
+        //Unfreezing so the game can use it's physics engine again
+        rb.freezeRotation = false;
+    }
+}
